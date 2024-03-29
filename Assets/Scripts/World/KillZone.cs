b80354c6f264b549536b62346f42201c53f2245a
GAME1.1 <GAME1.1@ectls.local>
2024-03-29 14:41:39 +0100

    Scrolling background

--- Assets/Scripts/World/KillZone.cs
+++ Assets/Scripts/World/KillZone.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KillZone : MonoBehaviour
+public class Background : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D col)
     {
---
+++ Assets/Scripts/World/LoopingBackground.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopingBackground : MonoBehaviour
+{
+    private Transform SpriteChild;
+    private float Width;
+    private float OriginX;
+
+    [SerializeField] private float SpeedMultiplier = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OriginX = transform.position.x;
+        int NbSpriteChild = transform.childCount;
+
+        for (int i = 0; i < NbSpriteChild; i++)
+        {
+            SpriteChild = transform.GetChild(i);
+            Width = SpriteChild.GetComponent<SpriteRenderer>().bounds.size.x;
+            Instantiate(SpriteChild, new Vector3(SpriteChild.position.x + Width, SpriteChild.position.y, 0), Quaternion.identity, transform);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * SpeedMultiplier * Time.deltaTime);
+        if (transform.position.x < OriginX - Width)
+        {
+            transform.position = new Vector3(OriginX, transform.position.y, 0);
+        }
+
+    }
+}
