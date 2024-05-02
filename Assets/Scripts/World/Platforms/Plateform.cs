8edad9aa1081db447f546e6f7e0369514d1cff0f
GAME1.1 <GAME1.1@ectls.local>
2024-05-02 12:29:09 +0200

    Les plateformes sont implémentées

--- Assets/Scripts/World/Platforms/Plateform.cs
+++ Assets/Scripts/World/Platforms/Plateform.cs
@@ -5,16 +5,11 @@ using UnityEngine;
 public class Plateform : MonoBehaviour
 {
     protected Collider2D collider2D;
-    private float ChangeColState = 0.5f;
-    protected InputController MyInputAction;
-
+    
     // Start is called before the first frame update
     protected void Start()
     {
         collider2D = GetComponent<Collider2D>();
-
-        MyInputAction = new InputController();
-        MyInputAction.Player.Enable();
     }
 
     // Update is called once per frame
@@ -23,18 +18,4 @@ public class Plateform : MonoBehaviour
 
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player") && MyInputAction.Player.VerticalMove.IsPressed())
-        {
-            StartCoroutine(ChangeCollisionState());
-        }
-    }
-
-    private IEnumerator ChangeCollisionState()
-    {
-        collider2D.enabled = false;
-        yield return new WaitForSeconds(ChangeColState);
-        collider2D.enabled = true;
-    }
 }
--- Assets/Scripts/World/Platforms/Plateforme_Mobile.cs
+++ Assets/Scripts/World/Platforms/Plateforme_Mobile.cs
@@ -5,36 +5,25 @@ using UnityEngine;
 
 public class Plateforme_Mobile : Plateform
 {
-    protected Dictionary<Transform, Transform> Famille;
     // Start is called before the first frame update
     new void Start()
     {
         base.Start();
     }
 
-
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (TestTag(other.gameObject))
+        if (other.gameObject.CompareTag("Player"))
         {
-            Famille.Add(other.gameObject.transform, other.gameObject.transform.parent);
             other.gameObject.transform.SetParent(transform, true); // Conservation de la position de l'enfant par rapport au monde
         }
     }
 
     private void OnCollisionExit2D(Collision2D other)
     {
-        if (TestTag(other.gameObject))
+        if (other.gameObject.CompareTag("Player"))
         {
-            Transform AncienParent;
-            Famille.TryGetValue(other.gameObject.transform, out AncienParent);
-            other.gameObject.transform.SetParent(AncienParent, true); // Conservation de la position de l'enfant par rapport au monde
-            Famille.Remove(other.gameObject.transform);
+            other.gameObject.transform.parent = null; 
         }
-    }
-
-    private bool TestTag(GameObject go)
-    {
-        return (go.gameObject.CompareTag("Player") || go.gameObject.CompareTag("Ennemy"));
     }
 }
